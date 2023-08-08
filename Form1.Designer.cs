ab1d127238807ad064927f799c5849cb0e58fea7
inang <inang@INANGUCLU>
2023-08-08 23:23:56 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace _02.MarketDesktopApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(15F, 31F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(814, 527);
+            Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(6, 5, 6, 5);
+            Name = "MainForm";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace _02.MarketDesktopApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
